414b144119930dbe54c23697109c82413900530f
Madhav <mpdebug200@gmail.com>
2021-03-22 20:07:37 +0530

    initial commit

---
+++ project.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcome to flask"
+
+@app.route('/hello')
+def helloWorld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
